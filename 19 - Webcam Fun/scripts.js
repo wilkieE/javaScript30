ef4d0da5b0794c1aa30d160f65c1f1e4247e27d6
jyromaniac <wilkieejiroghene@gmail.com>
2021-02-15 11:47:22 +0200

    Created getVideo and paintToCanvas functions

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,32 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+       .then(localMediaStream => {
+           console.log(localMediaStream);
+           video.srcObject = localMediaStream;
+           video.play();
+       })
+       .catch(err => {
+           console.error('MERDE!', err);
+       });
+}
+function paintToCanvas(){
+    const height = video.videoHeight;
+    const width = video.videoWidth;
+    canvas.height = height;
+    canvas.width = width;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 10);
+}
+function takePhoto(){
+    snap.currentTime = 0;
+    snap.play();
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
